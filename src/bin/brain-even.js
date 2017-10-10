be5b88418d8351769a68a5de21d74c9923da62e3
Oleg Yarymov <yarymvsarge@gmail.com>
2017-10-10 22:41:43 +0300

    Step 4. Change naming-errors and some logic.

--- src/bin/brain-even.js
+++ src/bin/brain-even.js
@@ -1,4 +1,4 @@
 #!/usr/bin/env node
-import { evenGame } from '..';
+import { startEvenGame } from '..';
 
-evenGame();
+startEvenGame();
--- src/index.js
+++ src/index.js
@@ -2,36 +2,36 @@ import readlineSync from 'readline-sync';
 
 const start = () => {
   console.log('Welcome to the Brain Games!');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+  const playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${playerName}!`);
 };
 
-const evenGame = () => {
+const startEvenGame = () => {
   console.log('Welcome to the Brain Games!');
   console.log('Answer "yes" if number even otherwise answer "no".');
-  const name = readlineSync.question('May I have your name? ');
-  console.log(`Hello, ${name}!`);
+  const playerName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${playerName}!`);
 
   const maxNumber = 1000;
-  const toWin = 3;
-  const iter = (attemptsToWin) => {
-    if (attemptsToWin === 0) {
-      console.log(`Congratulations, ${name}!`);
+  const attemptsCount = 3;
+  const iter = (rightAnswersCount) => {
+    if (rightAnswersCount === attemptsCount) {
+      console.log(`Congratulations, ${playerName}!`);
       return;
     }
-    const curNum = Math.floor(Math.random() * maxNumber);
-    console.log(`Question: ${curNum}`);
-    const answer = readlineSync.question('Your answer: ');
-    if ((curNum % 2 === 0 && answer === 'yes') || (curNum % 2 !== 0 && answer === 'no')) {
+    const curNumber = Math.floor(Math.random() * maxNumber);
+    console.log(`Question: ${curNumber}`);
+    const userAnswer = readlineSync.question('Your answer: ');
+    const correctAnswer = curNumber % 2 === 0 ? 'yes' : 'no';
+    if (userAnswer === correctAnswer) {
       console.log('Correct!');
-      iter(attemptsToWin - 1);
+      iter(rightAnswersCount + 1);
     } else {
-      const correctAns = curNum % 2 === 0 ? 'yes' : 'no';
-      console.log(`'${answer}' is wrong answer ;(. Correct answer was '${correctAns}'`);
-      console.log(`Let's try again, ${name}!`);
+      console.log(`'${userAnswer}' is wrong answer ;(. Correct answer was '${correctAnswer}'`);
+      console.log(`Let's try again, ${playerName}!`);
     }
   };
-  return iter(toWin);
+  return iter(0);
 };
 
-export { start, evenGame };
+export { start, startEvenGame };
