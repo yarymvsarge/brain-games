9c728f4e01ddbd706f58357986c764a462a2279d
Oleg Yarymov <yarymvsarge@gmail.com>
2017-10-13 16:09:43 +0300

    change progression logic to functional

--- src/games/progression.js
+++ src/games/progression.js
@@ -3,33 +3,35 @@ import generateRandom from '../utils';
 import play from '..';
 
 const progressionLength = 10;
-const maxStartNumber = 100;
+const maxStartElement = 100;
 const maxProgressionStep = 10;
 
-const generateProgression = () => {
-  const startNumber = generateRandom(maxStartNumber);
+const generateProgression = (questionIndex) => {
+  const startElement = generateRandom(maxStartElement);
   const progressionStep = generateRandom(maxProgressionStep) + 1; // if it will be zero
-  const iter = (currentProgression) => {
-    if (currentProgression.length === progressionLength) {
-      return currentProgression;
+  const iter = (currentProgression, answer) => {
+    const currentLength = currentProgression.length;
+    const newIndex = currentLength;
+    if (currentLength === progressionLength) {
+      return cons(currentProgression.join(' '), String(answer));
     }
-    const lastIndex = currentProgression.length - 1;
-    const lastNumber = currentProgression[lastIndex];
-    const newNumber = lastNumber + progressionStep;
-    currentProgression.push(newNumber);
-    return iter(currentProgression);
+    const newElement = (currentLength * progressionStep) + startElement;
+    if (questionIndex === newIndex) {
+      const newProgression = currentProgression.concat('..');
+      const rightAnswer = newElement;
+      return iter(newProgression, rightAnswer);
+    }
+    const newProgression = currentProgression.concat(newElement);
+    return iter(newProgression, answer);
   };
-  const progression = iter([startNumber]);
-  return progression;
+  const progressionAndAnswer = iter([startElement], undefined);
+  return progressionAndAnswer;
 };
 
 const generateAnswerAndQuestion = () => {
-  const progression = generateProgression();
   const questionIndex = generateRandom(progressionLength - 1);
-  const answer = String(progression[questionIndex]);
-  progression[questionIndex] = '..';
-  const question = progression.join(' ');
-  return cons(question, answer);
+  const progressionAndAnswer = generateProgression(questionIndex);
+  return progressionAndAnswer;
 };
 
 const description = 'What number is missing in this progression?';
